96213a01f57d645e1906265fad41a4ee1391d33f
Dhanaallu <alludhananaidu@gmail.com>
2022-12-29 12:45:20 +0530

    dummy

---
+++ calc.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    return a+b
+
+
+def sub(a,b):
+    return a-b
+def mult(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+def percentage(a,b):
+    return (a/b)*100
+
+#hhhh
+
+# new changes
